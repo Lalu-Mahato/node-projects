7f2d52daf960cee8bfe426fb30d245b29a9487e8
Lalu-Mahato <135944020+Lalu-Mahato@users.noreply.github.com>
2023-06-23 16:22:02 +0530

    Infura IPFS file upload implementation

--- ipfs-sample/app.js
+++ ipfs-sample/app.js
@@ -25,4 +25,66 @@ app.post('/share', upload, async (req, res) => {
     }
 });
 
+
+const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
+
+const client = ipfsClient.create({
+    host: "ipfs.infura.io",
+    port: 5001,
+    protocol: "https",
+    headers: {
+        authorization: auth,
+    }
+});
+
+app.post('/upload', upload.single('avatar'), async (req, res) => {
+    try {
+        let data = Buffer.from(fs.readFileSync(req.file.path));
+
+        return new bluebird((resolve, reject) => {
+            client.add(data).then((response) => {
+                resolve(response)
+            });
+            fs.unlinkSync(req.file.path);
+        })
+            .then((x) => {
+                return res.send(x.path);
+            })
+            .catch((err) => {
+                res.send({ Error: err });
+            });
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+
+const uploadFileToIPFS = (data) => {
+    return new Promise((resolve, reject) => {
+        client.add(data)
+            .then(response => {
+                resolve(response);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
+};
+
+app.post('/share-file', upload.single('avatar'), async (req, res) => {
+    try {
+        let data = Buffer.from(fs.readFileSync(req.file.path));
+
+        uploadFileToIPFS(data)
+            .then(response => {
+                return res.send(response.path);
+            })
+            .catch(error => {
+                res.send({ Error: error });
+            });
+    } catch (err) {
+        return res.send(501, 'Error', err);
+    }
+});
+
 app.listen(port, () => console.log(`Server listening on port:${port}`));
